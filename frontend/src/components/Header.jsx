cebe2df5593fd8c4f8a25054ee4ed7cfdd75cf3b
Mahmood <mahmood.kn1526@gmail.com>
2023-07-09 22:24:27 +0330

    add header and footer

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,32 @@
+'use client';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+// import logo from '../assets/logo.png';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import Image from 'next/image';
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>
+            {/* <Image src={logo} alt='ProShop' /> */}
+            ProShop
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Nav.Link href='/cart'>
+                <FaShoppingCart /> Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <FaUser /> Login
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
