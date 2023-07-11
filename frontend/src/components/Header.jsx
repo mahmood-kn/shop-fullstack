215fe2aa672bdf713f606885535b96f7ef11c046
Mahmood <mahmood.kn1526@gmail.com>
2023-07-11 23:08:47 +0330

    add links

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,26 +1,33 @@
 'use client';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-// import logo from '../assets/logo.png';
+import logo from '../assets/logo.png';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import Image from 'next/image';
+import Link from 'next/link';
 const Header = () => {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>
-            {/* <Image src={logo} alt='ProShop' /> */}
-            ProShop
-          </Navbar.Brand>
+          <Link href='/'>
+            <Navbar.Brand as='span'>
+              <Image src={logo} alt='ProShop' />
+              ProShop
+            </Navbar.Brand>
+          </Link>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link href='/cart'>
-                <FaShoppingCart /> Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <FaUser /> Login
-              </Nav.Link>
+              <Link href='/cart'>
+                <Nav.Link as='span'>
+                  <FaShoppingCart /> Cart
+                </Nav.Link>
+              </Link>
+              <Link href='/login'>
+                <Nav.Link as='span'>
+                  <FaUser /> Login
+                </Nav.Link>
+              </Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,17 +1,18 @@
+import Link from 'next/link';
 import { Card } from 'react-bootstrap';
 
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-'>
-      <a href={`product/${product._id}`}>
+      <Link href={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top' />
-      </a>
+      </Link>
       <Card.Body>
-        <a href={`product/${product._id}`}>
+        <Link href={`/product/${product._id}`}>
           <Card.Title as='div'>
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
         <Card.Text as='h3'>${product.price}</Card.Text>
       </Card.Body>
     </Card>
