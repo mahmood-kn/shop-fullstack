e12263097c68273fc096cf4918567c6234e86f9c
Mahmood <mahmood.kn1526@gmail.com>
2023-07-14 20:52:29 +0330

    add mongoose and models

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 const port = process.env.PORT || 5000;
 import products from './data/products.js';
 
+connectDB();
 const app = express();
 
 app.get('/', (req, res) => {
