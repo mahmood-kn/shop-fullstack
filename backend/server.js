7f75058107e4eeffae16e9394355a253d387fb8e
Mahmood <mahmood.kn1526@gmail.com>
2023-07-23 23:37:04 +0330

    add protected routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
 const port = process.env.PORT || 5000;
@@ -14,6 +15,8 @@ const app = express();
 // Body Parse Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use(cors());
 
