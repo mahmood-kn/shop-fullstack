2fcc67717db998082e2e70ba94fe939e21983351
Mahmood <mahmood.kn1526@gmail.com>
2023-07-25 00:04:41 +0330

    clean up

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,7 +5,6 @@ import Product from '../models/productModel.js';
 // @route GET /api/products
 // @access Public
 const getProducts = asyncHandler(async (req, res) => {
-  console.log(req.cookies, 'mahmood');
   const products = await Product.find({});
   res.json(products);
 });
