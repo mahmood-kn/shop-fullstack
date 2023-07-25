3e8c4acfeb3a654d23d159c1e5e7713d7f1bb763
Mahmood <mahmood.kn1526@gmail.com>
2023-07-25 13:48:30 +0330

    add checkout steps

---
+++ frontend/src/components/CheckoutSteps.jsx
@@ -0,0 +1,49 @@
+import { Nav } from 'react-bootstrap';
+import Link from 'next/link';
+
+const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
+  return (
+    <Nav className='justify-content-center mb-4'>
+      <Nav.Item>
+        {step1 ? (
+          <Nav.Link as={Link} href='/login'>
+            Sign In
+          </Nav.Link>
+        ) : (
+          <Nav.Link disabled>Sign In</Nav.Link>
+        )}
+      </Nav.Item>
+      <Nav.Item>
+        {step2 ? (
+          <Nav.Link as={Link} href='/shipping'>
+            Shipping
+          </Nav.Link>
+        ) : (
+          <Nav.Link disabled>Shipping</Nav.Link>
+        )}
+      </Nav.Item>
+
+      <Nav.Item>
+        {step3 ? (
+          <Nav.Link as={Link} href='/payment'>
+            Payment
+          </Nav.Link>
+        ) : (
+          <Nav.Link disabled>Payment</Nav.Link>
+        )}
+      </Nav.Item>
+
+      <Nav.Item>
+        {step4 ? (
+          <Nav.Link as={Link} href='/placeorder'>
+            Place Order
+          </Nav.Link>
+        ) : (
+          <Nav.Link disabled>Place Order</Nav.Link>
+        )}
+      </Nav.Item>
+    </Nav>
+  );
+};
+
+export default CheckoutSteps;
--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -5,6 +5,7 @@ import { Form, Button } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { useRouter } from 'next/navigation';
 import { saveShippingAddress } from '@/redux/slices/cartSlice';
+import CheckoutSteps from '@/components/CheckoutSteps';
 
 const ShippingScreen = () => {
   const { shippingAddress } = useSelector((state) => state.cart);
@@ -30,6 +31,7 @@ const ShippingScreen = () => {
   };
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group>
