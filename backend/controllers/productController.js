889315b482a68694a059f577bfdfeb4a9f72aac1
Mahmood <mahmood.kn1526@gmail.com>
2023-07-28 20:31:03 +0330

    work reviews backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -23,7 +23,7 @@ const getProductById = asyncHandler(async (req, res) => {
 });
 
 // @desc    Create a product
-// @route Post /api/products
+// @route POST /api/products
 // @access Private/Admin
 const createProduct = asyncHandler(async (req, res) => {
   const product = new Product({
@@ -64,13 +64,45 @@ const updateProduct = asyncHandler(async (req, res) => {
 });
 
 // @desc    Delete a product
-// @route Post /api/products
+// @route POST /api/products
 // @access Private/Admin
 const deleteProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
   if (product) {
     await Product.deleteOne({ _id: product._id });
     res.status(200).json({ message: 'Product deleted' });
+  } else {
+    res.status(404);
+    throw new Error('Resource not found');
+  }
+});
+// @desc   Create a new review
+// @route POST /api/products/:id/reviews
+// @access Private/Admin
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (review) => review.ser.toString() === req.user._id.toString()
+    );
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error('Product already reviewed');
+    }
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+    product.reviews.push(review);
+    product.numReviews = product.reviews.length;
+    product.rating =
+      product.reviews.reduce((acc, review) => acc + review.rating, 0) /
+      product.reviews.length;
+    await product.save();
+    res.status(201).json({ message: 'Review added' });
   } else {
     res.status(404);
     throw new Error('Resource not found');
@@ -83,4 +115,5 @@ export {
   createProduct,
   updateProduct,
   deleteProduct,
+  createProductReview,
 };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,6 +6,7 @@ import {
   getProductById,
   getProducts,
   updateProduct,
+  createProductReview,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 router.route('/').get(getProducts).post(protect, admin, createProduct);
@@ -14,5 +15,6 @@ router
   .get(getProductById)
   .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
 
 export default router;
